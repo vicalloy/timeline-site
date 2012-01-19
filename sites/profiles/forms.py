e4745074759d112fb41a2fd202184849942a09e1
vicalloy <zbirder@gmail.com>
2012-01-19 17:24:59 +0800

    * finish user profile edit.

--- sites/profiles/forms.py
+++ sites/profiles/forms.py
@@ -1,18 +1,10 @@
-from django import forms
-from django.utils.translation import ugettext_lazy as _
+from userena.forms import SignupForm, AuthenticationForm, ChangeEmailForm
+from userena.utils import get_profile_model
+from django.contrib.auth.forms import PasswordChangeForm
 
-from userena.forms import SignupForm, AuthenticationForm
-from bootstrap.forms import BootstrapMixin
-
-USERNAME_RE = r'^\S+$'
-attrs_dict = {'class': 'required'}
+from bootstrap.forms import BootstrapMixin, BootstrapModelForm
 
 class BsSignupForm(SignupForm, BootstrapMixin):
-    username = forms.RegexField(regex=USERNAME_RE,
-                                max_length=30,
-                                widget=forms.TextInput(attrs=attrs_dict),
-                                label=_("Username"),
-                                error_messages={'invalid': _('Username must contain only letters, numbers, dots and underscores.')})
 
     def __init__(self, *args, **kw):
         super(BsSignupForm, self).__init__(*args, **kw)
@@ -23,3 +15,28 @@ class BsAuthenticationForm(AuthenticationForm, BootstrapMixin):
     def __init__(self, *args, **kw):
         super(BsAuthenticationForm, self).__init__(*args, **kw)
         self.__bootstrap__()
+
+class BsPasswordChangeForm(PasswordChangeForm, BootstrapMixin):
+
+    def __init__(self, *args, **kw):
+        super(BsPasswordChangeForm, self).__init__(*args, **kw)
+        self.__bootstrap__()
+
+class BsChangeEmailForm(ChangeEmailForm, BootstrapMixin):
+
+    def __init__(self, *args, **kw):
+        print "------------xxx"
+        super(BsChangeEmailForm, self).__init__(*args, **kw)
+        self.__bootstrap__()
+
+class BsEditProfileForm(BootstrapModelForm):
+
+    class Meta:
+        model = get_profile_model()
+        exclude = ['user']
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        profile = super(BsEditProfileForm, self).save(commit=commit)
+        user = profile.user
+        user.save()
+        return profile
--- sites/profiles/urls.py
+++ sites/profiles/urls.py
@@ -2,12 +2,26 @@ from django.conf.urls.defaults import *
 
 from userena import views as userena_views
 
-from profiles.forms import BsSignupForm, BsAuthenticationForm
+from profiles.forms import BsSignupForm
+from profiles.forms import BsAuthenticationForm
+from profiles.forms import BsEditProfileForm
+from profiles.forms import BsPasswordChangeForm
+from profiles.forms import BsChangeEmailForm
 
 urlpatterns = patterns('',
     url(r'^signup/$', userena_views.signup,
         {'signup_form': BsSignupForm}, name='userena_signup'),
     url(r'^signin/$', userena_views.signin,
         {'auth_form': BsAuthenticationForm}, name='userena_signin'),
+    url(r'^(?P<username>[\.\w]+)/edit/$',
+       userena_views.profile_edit,
+       {'edit_profile_form': BsEditProfileForm}, name='userena_profile_edit'),
+    # Change password
+    url(r'^(?P<username>[\.\w]+)/password/$',
+       userena_views.password_change,
+       {'pass_form': BsPasswordChangeForm}, name='userena_password_change'),
+    url(r'^(?P<username>[\.\w]+)/email/$',
+       userena_views.email_change,
+       {'email_form': BsChangeEmailForm}, name='userena_email_change'),
     (r'^', include('userena.urls')),
 )

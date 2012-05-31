f570bfd1f8522bede3ffb12ad5964214202512d1
vicalloy <zbirder@gmail.com>
2012-05-31 17:48:39 +0800

    modified:   timeline/templatetags/timeline_widget_tags.py

--- sites/timeline/templatetags/timeline_widget_tags.py
+++ sites/timeline/templatetags/timeline_widget_tags.py
@@ -1,20 +1,20 @@
 from django.template import Library
 
-from timeline.models import Timeline
+from timeline.models import get_all_timlines
 
 register = Library()
 
 @register.inclusion_tag('dummy.html')
 def tl_hot(template='timeline/widgets/hot.html'):
     return {'template': template,
-            'timelines': Timeline.objects.order_by('-num_views')[:5] }
+            'timelines': get_all_timlines().order_by('-num_views')[:5] }
 
 @register.inclusion_tag('dummy.html')
 def tl_last(template='timeline/widgets/last.html'):
     return {'template': template,
-            'timelines': Timeline.objects.order_by('-updated_on')[:5] }
+            'timelines': get_all_timlines().order_by('-updated_on')[:5] }
 
 @register.inclusion_tag('dummy.html')
 def tl_recommend(template='timeline/widgets/recommend.html'):
     return {'template': template,
-            'timelines': Timeline.objects.order_by('-rec_on')[:5] }
+            'timelines': get_all_timlines().order_by('-rec_on')[:5] }

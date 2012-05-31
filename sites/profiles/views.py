433409646e4c80fb9eb81dcbb2ee12da65883caa
vicalloy <zbirder@gmail.com>
2012-05-31 13:08:23 +0800

    add my timeline list

---
+++ sites/profiles/views.py
@@ -0,0 +1,11 @@
+from userena.views import profile_detail as __profile_detail
+
+from timeline.models import get_all_timlines
+
+def profile_detail(request, username):
+    ctx = {}
+    if request.user.username == username:
+        ctx['timelines'] = request.user.timeline_set.order_by('-updated_on')
+    else:
+        ctx['timelines'] = get_all_timlines().filter(created_by=request.user)
+    return __profile_detail(request, username, extra_context=ctx)

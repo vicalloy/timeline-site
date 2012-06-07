0e00551f03d0444a3adec7b0e02cc4246dbd9573
vicalloy <zbirder@gmail.com>
2012-06-07 10:45:52 +0800

    bug fix

--- sites/profiles/views.py
+++ sites/profiles/views.py
@@ -1,11 +1,15 @@
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+
 from userena.views import profile_detail as __profile_detail
 
 from timeline.models import get_all_timlines
 
 def profile_detail(request, username):
     ctx = {}
-    if request.user.username == username:
-        ctx['timelines'] = request.user.timeline_set.exclude(status='del').order_by('-updated_on')
+    view_user = get_object_or_404(User, username=username)
+    if request.user.is_authenticated() and request.user.username == username:
+        ctx['timelines'] = view_user.timeline_set.exclude(status='del').order_by('-updated_on')
     else:
-        ctx['timelines'] = get_all_timlines().filter(created_by=request.user)
+        ctx['timelines'] = get_all_timlines().filter(created_by=view_user)
     return __profile_detail(request, username, extra_context=ctx)

fe9a91789ec3c2ced97ab501e07a522e89bb7bfd
vicalloy <zbirder@gmail.com>
2012-07-04 17:51:21 +0800

    add unittest

---
+++ sites/timeline/tests/__init__.py
@@ -0,0 +1,56 @@
+# -*- coding: UTF-8 -*-
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+from timeline.models import Timeline
+
+class ViewsBaseCase(TestCase):
+
+    fixtures = ['timeline/tests/fixtures/timeline_users.json', 
+            'timeline/tests/fixtures/timeline_timeline.json']
+
+    def setUp(self):
+        self.admin = User.objects.create_superuser(
+            username='admin', email='admin@example.com', password='admin')
+        self.user = User(username="user", email="user@localhost", is_staff=False, is_superuser=False)
+        self.user.set_password('user')
+        self.user.save()
+
+class ViewsTest(ViewsBaseCase):
+
+    def setUp(self):
+        super(ViewsTest, self).setUp()
+        assert self.client.login(username='user', password='user')
+
+    def test_idx(self):
+        resp = self.client.get(reverse('timeline_idx'))
+        self.assertEquals(resp.status_code, 200)
+
+    def test_hot(self):
+        resp = self.client.get(reverse('timeline_hot'))
+        self.assertEquals(resp.status_code, 200)
+
+    def test_recommend(self):
+        resp = self.client.get(reverse('timeline_recommend'))
+        self.assertEquals(resp.status_code, 200)
+
+    def test_last(self):
+        resp = self.client.get(reverse('timeline_last'))
+        self.assertEquals(resp.status_code, 200)
+
+    def test_random(self):
+        resp = self.client.get(reverse('timeline_random'))
+        self.assertEquals(resp.status_code, 200)
+
+    def test_tags(self):
+        resp = self.client.get(reverse('timeline_tags'))
+        self.assertEquals(resp.status_code, 200)
+
+    def test_tag(self):
+        print "==========", User.objects.count()
+        print "==========", Timeline.objects.count()
+        timeline = Timeline.objects.get(pk=1)
+        timeline.tags.add('test', 'tag')
+        resp = self.client.get(reverse('timeline_tag', args=('test', )))
+        self.assertEquals(resp.status_code, 200)
